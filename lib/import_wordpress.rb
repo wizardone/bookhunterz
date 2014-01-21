29d77f8cd5e59f2c192cab5baafc74474f3082b0
Stefan Slaveykov <slaveykov@fidor.de>
2014-01-21 20:30:18 +0100

    added the module that will import wordpress shit

--- lib/import_wordpress.rb
+++ lib/import_wordpress.rb
@@ -1,3 +1,11 @@
 module ImportWordpress
-  
+  extend self
+
+  EXPORT_PATH = Rails.root
+  EXPORT_FILE = "wordpress.2014-01-21.xml"
+
+  def start(filename = nil)
+    filename = "#{EXPORT_PATH}/#{EXPORT_FILE}" unless filename.present?
+    file = Nokogiri::XML(File.open(filename))
+  end
 end
