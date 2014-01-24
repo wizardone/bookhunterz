4b632d4fa3830d9566aab64c87bff8bfb8a2258d
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2014-01-24 11:07:55 +0100

    added remember me logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
     if user
-      session[:user_id] = user.id
+      session[:user_id] = user.id if remember_me?
       redirect_to root_path, notice: "Logged in"
     else
       flash[:error] = "No user matches those credentials"
       render :new
     end
   end
+
+  private
+    def remember_me?
+      return params[:remember_me].present?
+    end
 end
