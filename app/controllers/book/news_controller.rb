dd7d90a0c2261425f79bce5cf59813661fd4f201
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2014-01-30 14:53:27 +0200

    nicer admin functionality, news working

--- app/controllers/book/news_controller.rb
+++ app/controllers/book/news_controller.rb
@@ -7,16 +7,28 @@ class Book::NewsController < ApplicationController
   def create
     @news = Book::News.new(news_params)
     @news.image = news_params[:image]
-    #@news.save!
-    #respond_with @news
     if @news.save
-      redirect_to news_index_path, notice: "News saved succesfully"
+      redirect_to book_news_index_path, notice: "News saved succesfully"
     else
       render :new
     end
   end
 
+  def update
+    @news = Book::News.find(params[:id])
+    if @news.update_attributes(news_params)
+      redirect_to book_news_path(@news), notice: "News edited successfully"
+    else
+      render :update
+    end
+  end
+
   def show
+    @news = Book::News.find(params[:id])
+  end
+
+  def edit
+    @news = Book::News.find(params[:id])
   end
 
   def new
