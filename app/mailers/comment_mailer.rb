459742f3ab3ed0a1333f73efecd7b4eadf1681c3
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2014-03-25 09:11:04 +0100

    removed host from mailer and set it in config

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,8 +1,10 @@
 class CommentMailer < ActionMailer::Base
-  default from: "admin@bookhunterz.com", to: ["wizard.oneandonly@gmail.com"]
+  include MailsHelper
+
+  default from: "admin@bookhunterz.com", to: ["wizard.oneandonly@gmail.com", "inni4ka@gmail.com"]
 
   def comment_added(resource)
-    @resource_url = book_review_url(resource, host: 'http://localhost')
+    @resource_url = book_review_url(resource)
     mail(subject: "Нов коментар беше добавен")
   end
 end
--- spec/mailers/comment_mailer_spec.rb
+++ spec/mailers/comment_mailer_spec.rb
@@ -6,7 +6,7 @@ describe CommentMailer do
       review = Book::Review.create!(review: "aaaa", book_name: "test")
       CommentMailer.comment_added(review).deliver
 
-      ActionMailer::Base.deliveries.last.to.should == ["wizard.oneandonly@gmail.com"]
+      ActionMailer::Base.deliveries.last.to.should == ["wizard.oneandonly@gmail.com", "inni4ka@gmail.com"]
       ActionMailer::Base.deliveries.last.subject.should == "Нов коментар беше добавен"
     end
   end
